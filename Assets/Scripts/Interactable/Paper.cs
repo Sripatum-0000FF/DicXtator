004ca31de7e84077c8a8461755fc5807e4e9f809
Teerapat Inwiset <152967250+BluePained@users.noreply.github.com>
2026-01-30 19:35:40 +0700

    PaperSetup

---
+++ Assets/Scripts/Interactable/Paper.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Paper : Entity
+{
+    public override void Interact(GameObject interactor)
+    {
+        print("Paper Interact");
+    }
+
+    public void PopupPaper()
+    {
+        
+    }
+}
