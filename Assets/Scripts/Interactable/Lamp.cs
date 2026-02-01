28b2345664e5896291236cee1a8e5bf89a783dee
KaWa <kawa.sleep.work@gmail.com>
2026-02-01 10:35:18 +0700

    Lamp_LightUpdate

--- Assets/Scripts/Interactable/Lamp.cs
+++ Assets/Scripts/Interactable/Lamp.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class Lamp : Entity
 {
+    public GameObject lightGameObject;
     public override void Interact(GameObject gameObject)
     {   
         base.Interact(gameObject);
         SoundManager.Instance.PlaySFX("LampClick", 1, 1);
+        lightGameObject.SetActive(!lightGameObject.activeSelf);
     }
 }
